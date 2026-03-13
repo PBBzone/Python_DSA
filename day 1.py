c2b4498951ee6d33aff9e80990d9b5f1a9d6837c
PBBzone <prajwalborkar006@gmail.com>
2026-03-13 13:10:48 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,61 @@
+name = input("what is your name:")
+age = input("your age?:")
+roll = input("what is roll no.:")
+department = input("what's os your department:")
+print("my name is ",name)
+print("my age is ",age)
+print("my roll",roll)
+print("my deparment is",department)
+
+age = 64
+name = "prajwal"
+money = 80000
+temp = 42.3
+print(type(name))
+print(type(money))
+print(type(age))
+print(type(temp))
+
+num1 = input("enter a number")
+num2 = input("enter a number")
+
+num3 = int(num1) + int(num2)
+print("addition will be",num3)
+len = input("enter lenght :")
+hi = input("what is hight:")
+rect= int(len)*int(hi)
+
+print("area of rectangle is ",rect)
+
+cel = float(input("enter cesius:"))
+f=(cel*9/5)+32
+print(f)
+
+SI = (p*r*t)/100
+p=float(input("enter principal"))
+r=float(input("enter rate"))
+t=float(input("enter time"))
+si=(p*r*t)/100
+print("simple interst =",si)
+
+
+a = 10 
+b = 20 
+a,b=b,a
+print("a=",a)
+print("b=",b)
+
+#minuts to housrs
+
+sec = float(input("give seconds"))
+hourse = int(sec)*60
+print("hourse")
+
+meter = float(input("give meters"))
+hourse = int(meter)*1000
+print("hourse")
+
+#find your current age
+year = float(input("what is your birth year:"))
+yearss = int(year)
+
